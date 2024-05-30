69c4b5aa67aab918237fd058ac2dbadae1eea53c
Ola <lanreprincess@gmail.com>
2024-05-30 18:24:25 +0100

    modified MealCard styling

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -1,15 +1,23 @@
+import { Link } from "react-router-dom";
+
 export function MealCard({ meal }) {
-  const ingredients = [];
-  for (let i = 1; i <= 20; i++) {
-    const ingredient = meal[`strIngredient${i}`];
-    const measure = meal[`strMeasure${i}`];
-    if (ingredient) {
-      ingredients.push(`${measure} ${ingredient}`);
-    }
-  }
+  // const ingredients = [];
+  // for (let i = 1; i <= 20; i++) {
+  //   const ingredient = meal[`strIngredient${i}`];
+  //   const measure = meal[`strMeasure${i}`];
+  //   if (ingredient) {
+  //     ingredients.push(`${measure} ${ingredient}`);
+  //   }
+  // }
   return (
     <>
-      <div className="border border-gray-300 rounded p4 mt-10">
+      <div
+        className="border rounded-md p4 mt-10"
+        style={{
+          borderWidth: "2px",
+          borderColor: "#A9C471",
+        }}
+      >
         {/* image div */}
         <div className=" flex justify-center items-center mt-4">
           <img
@@ -19,25 +27,33 @@ export function MealCard({ meal }) {
             width={"200px"}
           />
         </div>
-        <h3 className="font-bold text-xl">{meal.strMeal}</h3>
-        <p className="text-start ml-2 italic">
+        <Link
+          to="/details"
+          className="font-bold text-xl"
+          style={{
+            color: "#0F4122",
+          }}
+        >
+          {meal.strMeal}
+        </Link>
+        <p className="ml-2 italic text-center">
           <span>Category: </span>
           {meal.strCategory}
           <span className="ml-3">
             Origin: <span>{meal.strArea}</span>
           </span>
         </p>
-
-        <p className="text-start mt-2 ml-3">
+        {/* INGREDIENTS */}
+        {/* <p className="text-start mt-2 ml-3">
           <span className="text-lg font-semibold">Ingredients: </span> <br />
           {ingredients.map((ingredient, index) => (
             <span key={index} className="block">
               {ingredient}
             </span>
           ))}
-        </p>
+        </p> */}
 
-        <a href="">Instruction</a>
+        {/* INSTRUCTIONS */}
         {/* <p className="mt-3">
           <h3 className="font-bold text-xl text-start">Instructions</h3>
           <p className="text-justify ">{meal.strInstructions}</p>
