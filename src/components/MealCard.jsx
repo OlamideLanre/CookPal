d3d01fa504a8550206a623a1ee732fb75ee21be1
OlamideLanre <lanreprincess@gmail.com>
2024-10-07 20:50:37 +0100

    Navbar component

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -1,41 +1,51 @@
 import { Link } from "react-router-dom";
+import { HeartOutlined, BookOutlined, PlusOutlined } from "@ant-design/icons";
 
 export function MealCard({ meal }) {
   return (
     <>
-      <div
-        className="border rounded-md p4 mt-10 mealImage"
-        style={{
-          borderWidth: "2px",
-          borderColor: "#A9C471",
-        }}
-      >
+      <div className="border p4 mt-10 mealImage rounded-2xl card">
         {/* image div */}
-        <div className=" flex justify-center items-center mt-4">
+        <div className=" flex justify-center items-center mt-4 gap-6">
           <img
             src={meal.strMealThumb}
             alt={meal.strMeal}
             className="mb-2 rounded-md"
-            width={"200px"}
+            width={"150px"}
           />
         </div>
-        <Link
-          key={meal.idMeal}
-          to={`/details/${meal.idMeal}`}
-          className="font-bold text-xl"
-          style={{
-            color: "#0F4122",
-          }}
-        >
-          {meal.strMeal}
-        </Link>
-        <p className="ml-2 italic text-center">
+        <div className="flex justify-between">
+          <p className="font-bold text-xl px-5">{meal.strMeal}</p>
+          <div className="icons">
+            <div title="add to favorites" className="fav">
+              <HeartOutlined className="icon-child" />
+            </div>
+            <div title="add to bookmark" className="bookmark">
+              <PlusOutlined className="icon-child" />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-between py-2 px-5">
+          <Link
+            key={meal.idMeal}
+            to={`/details/${meal.idMeal}`}
+            className="view"
+            style={{
+              color: "#0F4122",
+            }}
+          >
+            View recipe
+          </Link>
+        </div>
+
+        {/* <p className="ml-2 italic text-center">
           <span>Category: </span>
           {meal.strCategory}
           <span className="ml-3">
             Origin: <span>{meal.strArea}</span>
           </span>
-        </p>
+        </p> */}
       </div>
     </>
   );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import { SearchOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+export function NavBar() {
+  return (
+    <>
+      <nav className="">
+        <div className="flex justify-around items-center p-4 nav-container">
+          <div className="logo flex gap-3">
+            <img src="cook-book.png" alt="logo" srcset="" width={"40px"} />
+            <div>
+              <h2 className="text-3xl font-bold tracking-wider text-yellow-500">
+                CookPal
+              </h2>
+            </div>
+          </div>
+
+          <div>
+            <input
+              type="text"
+              className="py-2 pr-20 pl-5 mr-3 outline-none border border-gray-300 rounded-md"
+              placeholder="find recipe..."
+            />
+            <SearchOutlined className="cursor-pointer" />
+          </div>
+          <div className="pages">
+            <Link>favorites</Link>
+            <Link>bookmark</Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
